382ef47a67b1320ad356375b4816ed3208430f59
Zhogina Lidya <anel1da@yandex.ru>
2021-01-28 23:29:16 +0300

    js popup added

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+//закрытие и открытие попап
 const popup = document.querySelector(".popup");
 const popupOpenButton = document.querySelector(".profile__edit-button");
 const popupCloseButton = popup.querySelector(".popup__close-button");
@@ -9,33 +10,21 @@ const togglePopup = function () {
 popupOpenButton.addEventListener("click", togglePopup);
 popupCloseButton.addEventListener("click", togglePopup);
 
-// Находим форму в DOM
-let formElement = document.querySelector(".form")// Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector(".popup__input_name") // Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector(".popup__input_job")// Воспользуйтесь инструментом .querySelector()
+//редактирование информации профиля
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+let formElement = document.querySelector(".form")
+let nameInput = formElement.querySelector(".popup__input_name")
+let jobInput = formElement.querySelector(".popup__input_job")
+
 function formSubmitHandler(evt) {
     evt.preventDefault();
 
+    let nameParagraph = document.querySelector(".profile__name");
+    let jobParagraph = document.querySelector(".profile__job");
 
-    const nameParagraph = document.querySelector(".profile__name");
-    console.log(nameInput.value);
     nameParagraph.textContent = nameInput.value;
-
-    const jobParagraph = document.querySelector(".profile__job");
-    console.log(jobInput.value);
     jobParagraph.textContent = jobInput.value;
-}
 
-// Эта строчка отменяет стандартную отправку формы.
-// Так мы можем определить свою логику отправки.
-// О том, как это делать, расскажем позже.
-// Получите значение полей jobInput и nameInput из свойства value
-// Выберите элементы, куда должны быть вставлены значения полей
-// Вставьте новые значения с помощью textContent
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+    popup.classList.toggle("popup_opened");
+}
 formElement.addEventListener('submit', formSubmitHandler);
