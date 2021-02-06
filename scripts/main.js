f2a402dbbcef23e87c4c949f8636ba37a35f4d4d
Lidya <anel1da@yandex.ru>
2021-02-06 13:56:15 +0300

    template element developing

--- scripts/main.js
+++ scripts/main.js
@@ -9,14 +9,14 @@ let nameInput = formElement.querySelector(".popup__input_type_name")
 let jobInput = formElement.querySelector(".popup__input_type_job")
 let nameParagraph = document.querySelector(".profile__name");
 let jobParagraph = document.querySelector(".profile__job");
-let closePopup = function () { 
-  popup.classList.remove("popup_opened");
+let closePopup = function() {
+    popup.classList.remove("popup_opened");
 }
-let openPopup = function () { 
+let openPopup = function() {
     popup.classList.add("popup_opened");
     nameInput.value = nameParagraph.textContent;
     jobInput.value = jobParagraph.textContent;
-  }
+}
 
 
 //отправка формы редактирования информации профиля
@@ -33,5 +33,60 @@ function submitForm(evt) {
 popupOpenButton.addEventListener("click", openPopup);
 popupCloseButton.addEventListener("click", closePopup);
 
-//слушатели  - отпрака формы
+//слушатели  - отправка формы
 formElement.addEventListener('submit', submitForm);
+
+
+const placesContainer = document.querySelector(".places");
+const templatePlace = document.querySelector("template");
+const initialCards = [{
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+        alt: "Гора Архыз"
+
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+        alt: "Фото реки зимой"
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+        alt: "Панельные дома"
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+        alt: "Горный пейзаж"
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+        alt: "Линия железной дороги в лесу"
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+        alt: "Скалистый берег озера Байкал"
+    }
+];
+
+
+function getPlace(item) {
+    const newPlace = templatePlace.content.cloneNode(true);
+    const headerPlace = newPlace.querySelector(".place__title");
+    const imagePlace = newPlace.querySelector(".place__image");
+    headerPlace.textContent = item.name;
+    imagePlace.src = item.link;
+    imagePlace.alt = item.alt;
+
+    return newPlace;
+}
+
+function render() {
+    const initialPlaces = initialCards.map(getPlace)
+    placesContainer.append(...initialPlaces);
+
+}
+render()
